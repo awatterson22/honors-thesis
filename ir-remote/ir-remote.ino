3e492109d206d6976bae1f98846eb1e70291f1c8
awatterson22 <abbywatterson19@yahoo.com>
2022-03-29 23:30:24 -0400

    transmitter code

--- ir-remote/ir-remote.ino
+++ ir-remote/ir-remote.ino
@@ -1,23 +1,13 @@
-#include <IRremote.h>         // use the IRRemote.h
-const int irReceiverPin = 2;  // the SIG of receiver module attach to pin2
-IRrecv irrecv(irReceiverPin); // Creates a variable of type IRrecv
-decode_results results;       // define resultsas
+#include <IRremote.h> // Now using PIN 9
+
+IRsend irsend;
 
 void setup()
 {
-    Serial.begin(9600);  // initialize serial,baudrate is 9600
-    irrecv.enableIRIn(); // enable ir receiver module
 }
 
 void loop()
 {
-    if (irrecv.decode(&results)) // if the ir receiver module receiver data
-    {
-        Serial.print("irCode: ");         // print "irCode: "
-        Serial.print(results.value, HEX); // print the value in hexdecimal
-        Serial.print(",  bits: ");        // print" , bits: "
-        Serial.println(results.bits);     // print the bits
-        irrecv.resume();                  // Receive the next value
-    }
-    delay(600); // delay 600ms
+    irsend.sendNECRaw(0xFF00FF, 32); // The code 'FF00FF' is the received diffuser
+    delay(2000);
 }
